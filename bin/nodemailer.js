cafb50daf56750d6b3d71644e567b8d1711c3087
fardog <code@fardogllc.com>
2014-08-31 21:28:30 -0700

    move to minimist for commandline parsing

--- bin/nodemailer.js
+++ bin/nodemailer.js
@@ -1,31 +1,45 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var colors = require('colors');
 var Cli = require('../lib/cli.js');
-var cli = new Cli().parse(process.argv.slice(2));
-var options = cli.options;
 
-var Mailer = require('../lib/mailer');
-var mailer = new Mailer(options);
-
-// build the mail data object
-var data = {
-  from: options.from,
-  to: options.to,
-  cc: options.cc,
-  bcc: options.bcc,
-  replyTo: options.replyTo,
-  subject: options.subject,
-  text: options.body ? { path: options.body } : options.bodyText,
-  attachments: options.attachment ? options.attachment.map(function (attachment) { return { path: attachment }; }) : null
-};
-
-
-mailer.sendMail(data, function(err, result) {
+var cli = new Cli().parse(process.argv.slice(2), function(err, message, options) {
   if (err) {
-    console.error(err);
+    console.error('\nYou had errors in your syntax. Use --help for further information.'.red);
+    err.forEach(function (e) {
+      console.error(e.message);
+    });
+  }
+  else if (message) {
+    console.log(message);
   }
   else {
-    console.log("Sent successfully: " + result.response);
+
+    var Mailer = require('../lib/mailer');
+    var mailer = new Mailer(options);
+
+    // build the mail data object
+    var data = {
+      from: options.from,
+      to: options.to,
+      cc: options.cc,
+      bcc: options.bcc,
+      replyTo: options.replyTo,
+      subject: options.subject,
+      text: options.body ? { path: options.body } : options.bodyText,
+      attachments: options.attachment ? options.attachment.map(function (attachment) { return { path: attachment }; }) : null
+    };
+
+
+    mailer.sendMail(data, function(err, result) {
+      if (err) {
+        console.error(err);
+      }
+      else {
+        console.log("Sent successfully: " + result.response);
+      }
+    });
   }
+
 });
